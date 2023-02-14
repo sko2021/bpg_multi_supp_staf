ffb06a39d9ad65d40c204e7aeede1d5f1f3d4865
sko2021 <86480534+sko2021@users.noreply.github.com>
2023-02-14 11:19:29 -0500

    Update urls.py

--- bpg/urls.py
+++ bpg/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [    
     path('', views.init, name='bpg'),
-    path('update/<str:user_id>/<str:object_id>/<str:app_name>/<str:sup_id>/<str:sup_name>',views.update_user_details,name='update'),
+    path('update/<str:user_id>/<str:object_id>/<str:app_name>/<str:sup_id>/<str:sup_name>/',views.update_user_details,name='update'),
     path('logout', views.logout, name='bpg-logout'),
 ]
